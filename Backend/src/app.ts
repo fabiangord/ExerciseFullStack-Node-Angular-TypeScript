3552cf2017d9538d56a78e1803d880e231b9a893
fabiangord <fabian_0227@hotmail.com>
2023-06-30 00:13:25 -0500

    Se realizan ajustes al back y a los cors

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -2,9 +2,17 @@ import express from 'express'
 import routesInfo from './routes/info.routes'
 import 'dotenv/config'
 import './db'
+import cors from 'cors'
 
 const app = express()
 const PORT = process.env.PORT
+
+const corsOptions = {
+  origin: 'http://localhost:4200',
+  optionsSuccessStatus: 200 
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json())
 
